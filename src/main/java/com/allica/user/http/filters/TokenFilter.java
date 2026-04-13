42830c191c0a353ee248c400456c37e03c2c7829
adarshnambiar <adarsh.nambiar1@jsw.in>
2026-04-13 17:50:50 +0530

    cors config fix

--- src/main/java/com/allica/user/http/filters/TokenFilter.java
+++ src/main/java/com/allica/user/http/filters/TokenFilter.java
@@ -39,10 +39,13 @@ public class TokenFilter extends OncePerRequestFilter {
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
     String apiKey = request.getHeader(X_API_KEY);
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      filterChain.doFilter(request, response);
+      return;
+    }
     if (Objects.nonNull(apiKey) && apiKey.equalsIgnoreCase(validApiKey)) {
       filterChain.doFilter(request, response);
     } else {
-
       ErrorResponseDTO errorResponseDto =
           ErrorResponseDTO.builder().code(HttpStatus.UNAUTHORIZED).message("unauthorized").build();
       UserServiceResponse<Object> paymentServiceResponse =
--- src/test/java/com/allica/user/utility/ResponseUtilTest.java
+++ src/test/java/com/allica/user/utility/ResponseUtilTest.java
@@ -1,5 +1,7 @@
 package com.allica.user.utility;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.allica.user.dto.ErrorResponseDTO;
 import com.allica.user.dto.UserServiceResponse;
 import com.allica.user.utils.ResponseUtil;
@@ -7,40 +9,38 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class ResponseUtilTest {
-    @Test
-    @DisplayName("success should create a successful response object")
-    void success_shouldCreateSuccessfulResponse() {
-        String message = "Operation successful";
-        String data = "Test Data";
+  @Test
+  @DisplayName("success should create a successful response object")
+  void success_shouldCreateSuccessfulResponse() {
+    String message = "Operation successful";
+    String data = "Test Data";
 
-        UserServiceResponse<String> response = ResponseUtil.success(message, data);
+    UserServiceResponse<String> response = ResponseUtil.success(message, data);
 
-        assertNotNull(response);
-        assertTrue(response.getSuccess());
-        assertEquals(message, response.getMessage());
-        assertEquals(data, response.getData());
-        assertNull(response.getError());
-    }
+    assertNotNull(response);
+    assertTrue(response.getSuccess());
+    assertEquals(message, response.getMessage());
+    assertEquals(data, response.getData());
+    assertNull(response.getError());
+  }
 
-    @Test
-    @DisplayName("failure should create a failure response object")
-    void failure_shouldCreateFailureResponse() {
-        String message = "Operation failed";
-        ErrorResponseDTO error =
-                ErrorResponseDTO.builder()
-                        .code(HttpStatus.BAD_REQUEST)
-                        .message("Invalid input provided")
-                        .build();
+  @Test
+  @DisplayName("failure should create a failure response object")
+  void failure_shouldCreateFailureResponse() {
+    String message = "Operation failed";
+    ErrorResponseDTO error =
+        ErrorResponseDTO.builder()
+            .code(HttpStatus.BAD_REQUEST)
+            .message("Invalid input provided")
+            .build();
 
-        UserServiceResponse<Void> response = ResponseUtil.failure(message, error);
+    UserServiceResponse<Void> response = ResponseUtil.failure(message, error);
 
-        assertNotNull(response);
-        assertFalse(response.getSuccess());
-        assertEquals(message, response.getMessage());
-        assertEquals(error, response.getError());
-        assertNull(response.getData());
-    }
+    assertNotNull(response);
+    assertFalse(response.getSuccess());
+    assertEquals(message, response.getMessage());
+    assertEquals(error, response.getError());
+    assertNull(response.getData());
+  }
 }
